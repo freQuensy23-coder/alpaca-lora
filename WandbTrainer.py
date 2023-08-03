c53635010b0bbe02e789184b6c889c0001d35109
freQuensy23-coder <mametevalex@gmail.com>
2023-08-03 03:05:11 +0300

    [IMP] save global step

--- WandbTrainer.py
+++ WandbTrainer.py
@@ -9,10 +9,12 @@ class WandbTrainer(transformers.Trainer):
     def __init__(self, wandb_logger, **kwargs):
         super().__init__(**kwargs)
         self.wandb_logger = wandb_logger
+        self.step = 0
 
     def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+        self.step += 1
         loss = super().training_step(model, inputs)
-        log = {"train_loss": loss.item(), "epoch": int(self.state.epoch), "step": int(self.state.global_step)}
+        log = {"train_loss": loss.item(), "epoch": int(self.state.epoch), "step": self.step}
         self.wandb_logger.log(log)
         print(log)
         # Возвращаем loss
